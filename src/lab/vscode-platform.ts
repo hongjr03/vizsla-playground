e52f3101607065679f83463ed21780c1475c4ab4
hongjr03 <hongjr03@gmail.com>
2026-05-26 21:02:50 +0800

    fix: register default VS Code extension API

--- src/lab/vscode-platform.ts
+++ src/lab/vscode-platform.ts
@@ -1,12 +1,28 @@
 import "vscode/localExtensionHost";
 import { initialize, type IWorkbenchConstructionOptions } from "@codingame/monaco-vscode-api";
+import { ExtensionHostKind, registerExtension } from "@codingame/monaco-vscode-api/extensions";
 import { waitServicesReady } from "@codingame/monaco-vscode-api/lifecycle";
 import { URI } from "@codingame/monaco-vscode-api/vscode/vs/base/common/uri";
 
 let startPromise: Promise<void> | undefined;
 
+const defaultApiExtension = registerExtension(
+  {
+    name: "vizsla-playground-client",
+    publisher: "vizsla",
+    version: "0.0.0",
+    engines: {
+      vscode: "*",
+    },
+  },
+  ExtensionHostKind.LocalProcess,
+  { system: true },
+);
+
 export function startVizslaVscodePlatform(): Promise<void> {
-  startPromise ??= initialize({}, undefined, workspaceConfiguration()).then(() => waitServicesReady());
+  startPromise ??= initialize({}, undefined, workspaceConfiguration())
+    .then(() => waitServicesReady())
+    .then(() => defaultApiExtension.setAsDefaultApi());
   return startPromise;
 }
 
